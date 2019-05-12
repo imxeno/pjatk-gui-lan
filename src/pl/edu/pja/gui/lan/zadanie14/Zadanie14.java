2ae83d44e6b3de6a1c53cf26454f56b0d9d721c1
Piotr Adamczyk <xeno@xeno.yt>
2019-05-12 12:23:47 +0200

    setDefaultCloseOperation w Zadaniu14

--- src/pl/edu/pja/gui/lan/zadanie14/Zadanie14.java
+++ src/pl/edu/pja/gui/lan/zadanie14/Zadanie14.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class Zadanie14 {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JTextArea textArea = new JTextArea();
         textArea.setBackground(Color.GREEN);
         textArea.setForeground(Color.RED);
